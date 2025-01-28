d4d1bbae9994e314146f2079fc5cbc2f8ce59b5a
ISABELLY LIMA DA SILVA <24122387@CFP127ALUNO.LOCAL>
2025-01-28 10:37:31 -0300

    js ok

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict"
+//"use strict" serve para corrigir os erros básicos
+
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocarCor () {
+
+    const cor = document.getElementById('cor').value
+    
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+botaoTrocarCor.addEventListener('click', trocarCor)
